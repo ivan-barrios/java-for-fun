2068ff6b8b876749cde679209934e00c98582c37
Ivan Barrios <ivanbar8112003@gmail.com>
2023-04-06 12:43:37 -0300

    Added arbol files

---
+++ Programacion3/src/prog3/arbol/ArbolBinario.java
@@ -0,0 +1,129 @@
+package prog3.arbol;
+
+import prog3.listagenerica.*;
+
+public class ArbolBinario<T> {
+	private T dato;
+	private ArbolBinario<T> hijoIzquierdo;   
+	private ArbolBinario<T> hijoDerecho; 
+
+	
+	public ArbolBinario() {
+		super();
+	}
+
+	public ArbolBinario(T dato) {
+		this.dato = dato;
+	}
+
+	/*
+	 * getters y setters
+	 * 
+	 */
+	public T getDato() {
+		return dato;
+	}
+
+	public void setDato(T dato) {
+		this.dato = dato;
+	}
+	
+
+	public ArbolBinario<T> getHijoIzquierdo() {
+		return this.hijoIzquierdo;
+	}
+
+	public ArbolBinario<T> getHijoDerecho() {
+		return this.hijoDerecho;
+
+	}
+
+	public void agregarHijoIzquierdo(ArbolBinario<T> hijo) {
+		this.hijoIzquierdo = hijo;
+	}
+
+	public void agregarHijoDerecho(ArbolBinario<T> hijo) {
+		this.hijoDerecho = hijo;
+	}
+
+	public void eliminarHijoIzquierdo() {
+		this.hijoIzquierdo = null;
+	}
+
+	public void eliminarHijoDerecho() {
+		this.hijoDerecho = null;
+	}
+
+	public boolean esVacio() {
+		return this.getDato() == null && !this.tieneHijoIzquierdo() && !this.tieneHijoDerecho();
+	}
+
+	public boolean esHoja() {
+		return (!this.tieneHijoIzquierdo() && !this.tieneHijoDerecho());
+
+	}
+
+	@Override
+	public String toString() {
+		return this.getDato().toString();
+	}
+
+	 
+	public boolean tieneHijoIzquierdo() {
+		return this.hijoIzquierdo!=null;
+	}
+
+	 
+	public boolean tieneHijoDerecho() {
+		return this.hijoDerecho!=null;
+	}
+
+	
+
+	public boolean esLleno() {
+		return false;
+	}
+
+	 boolean esCompleto() {
+		return false;
+	}
+
+	
+	// imprime el arbol en preorden  
+	public void printPreorden() {
+		
+	}
+
+	// imprime el arbol en postorden
+	public void printInorden() {
+		
+	}
+	
+	// imprime el arbol en postorden
+	public void printPostorden() {
+		
+	}
+
+
+	public void recorridoPorNiveles() {
+		
+	}
+
+	
+
+	public ListaGenerica<T> frontera() {
+		ListaGenerica<T> l = new ListaGenericaEnlazada<T>();
+
+		return l;
+	}
+
+	
+	
+	
+	public int contarHojas() {
+		return 0;
+	}
+
+	
+
+}
