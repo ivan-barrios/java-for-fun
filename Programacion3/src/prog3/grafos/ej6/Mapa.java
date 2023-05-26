c5ab17463abd6f5dfbd8eede1efa730778f11575
Ivan Barrios <ivanbar8112003@gmail.com>
2023-05-26 00:22:20 -0300

    Estructura de ej6, me voy a comer jajaja

---
+++ Programacion3/src/prog3/grafos/ej6/Mapa.java
@@ -0,0 +1,28 @@
+package prog3.grafos.ej6;
+import prog3.grafos.*;
+import prog3.listagenerica.*;
+
+public class Mapa {
+	Grafo<String> mapaCiudades;
+	
+	ListaGenerica<String> devolverCamino (String ciudad1, String ciudad2) {
+		//do	
+	}
+	
+	ListaGenerica<String> devolverCaminoExceptuando (String ciudad1, String ciudad2,ListaGenerica<String> ciudades){
+		//do
+	}
+	
+	ListaGenerica<String> caminoMasCorto(String ciudad1, String ciudad2) {
+		//do
+	}
+	
+	ListaGenerica<String> caminoSinCargarCombustible(String ciudad1, String ciudad2, int tanqueAuto) {
+		//do
+	}
+	
+	ListaGenerica<String> caminoConMenorCargaDeCombustible (String ciudad1, String ciudad2, int tanqueAuto){
+		//do
+	}
+	
+}
